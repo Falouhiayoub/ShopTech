6758ee130038adcfde8d7cc66ca2c22ca691c583
Falouhiayoub <falouhiayoub9@gmail.com>
2025-12-18 12:24:49 +0100

    Add the product card to the home and the catalogue pages

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaHeart, FaShoppingCart } from 'react-icons/fa'
+import { useDispatch } from 'react-redux'
+import {addToCart} from "../features/cart/cartSlice"
+import {toggleWishlist} from "../features/wishlist/wishlistSlice"
+
+const ProductCard = ({product}) => {
+  const dispatch = useDispatch()
+  return (
+    <div className='card'>
+      <img src= {product.image} alt={product.name}/>
+      <h3>{product.name}</h3>
+      <p>{product.price} $</p>
+
+      <div className="actions">
+        <FaShoppingCart onClick={() => dispatchEvent(addToCart(product))}/>
+        <FaHeart onClick={() => dispatch(toggleWishlist(product))}/>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
