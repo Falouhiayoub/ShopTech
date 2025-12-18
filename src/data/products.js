232898f61672a4f566d35770c7cf1b1423f2027e
Falouhiayoub <falouhiayoub9@gmail.com>
2025-12-18 11:10:19 +0100

    Website Products

---
+++ src/data/products.js
@@ -0,0 +1,79 @@
+export const products = [
+    {
+        id: 1,
+        name: "Casque Bluetooth",
+        price: 120,
+        image: "./casque.png",
+        bestSelling: true,
+    },
+    {
+        id: 2,
+        name: "Smart Watch",
+        price: 200,
+        image: "./smart.png",
+        bestSelling: true,
+    },
+    {
+        id: 3,
+        name: "Phone Case",
+        price: 25,
+        image: "./Case.png",
+        bestSelling: true,
+    },
+    {
+        id: 4,
+        name: "Iphone 15 Pro",
+        price: 999,
+        image: "./Discover.png",
+        bestSelling: true,
+    },
+    {
+        id: 5,
+        name: "JBL Speaker",
+        price: 899,
+        image: "./JBL.png",
+        bestSelling: false,
+    },
+    {
+        id: 6,
+        name: "Car Mount",
+        price: 50,
+        image: "./Mount.png",
+        bestSelling: false,
+    },
+    {
+        id: 7,
+        name: "Camera lens",
+        price: 150,
+        image: "./Camera Attachments.png",
+        bestSelling: false
+    },
+    {
+        id: 8,
+        name: "Portable power",
+        price: 80,
+        image: "./Portable.png",
+        bestSelling: false
+    },
+    {
+        id: 9,
+        name: "Car Mount",
+        price: 50,
+        image: "./Mount.png",
+        bestSelling: false
+    },
+    {
+        id: 10,
+        name: "Iphone 14",
+        price: 799,
+        image: "./iphone.png",
+        bestSelling: false
+    },
+    {
+        id: 11,
+        name: "Air Pods",
+        price: 60,
+        image: "./YAQ.png",
+        bestSelling: false
+    }
+];
