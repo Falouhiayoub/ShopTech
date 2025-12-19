d01483566191b4355d5b07fcff35c15ed1e35378
Falouhiayoub <falouhiayoub9@gmail.com>
2025-12-19 13:09:41 +0100

    adding the Navbar and the Sidebar cart components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Catalogue from './pages/Catalogue'
+import Wishlist from './pages/Wishlist'
+import CartSidebar from './components/CartSidebar'
 import './index.css'
+import Navbar from './components/Navbar'
 
 function App() {
-
-
   return (
-    <>
-      <div></div>
-    </>
+    <BrowserRouter>
+    <Navbar/>
+    <CartSidebar />
+    
+      <Routes>
+        <Route path= "/" element ={<Home/>}/>
+        <Route path='/catalogue' element= {<Catalogue/>}/>
+        <Route path='/wishlist' element = {<Wishlist/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
